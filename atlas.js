791ea563f6d107d17f2fd96553132792b96ae5cf
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-01 15:07:58 +0530

    showing data into server 8989 port

--- atlas.js
+++ atlas.js
@@ -9,10 +9,22 @@ mongoose.connect(connect_str, { useNewUrlParser: true , useUnifiedTopology: true
 	.then( () => console.log("Connected successfully...") )
 	.catch( (err) => console.log(err) );
 
+const userSchema = new mongoose.Schema({
+        name: String,
+        age: Number,
+        city: String
+    });
+    
+const students = new mongoose.model("students", userSchema);
+
+app.get("/students", async(req,res) => {
+    let data = await students.find()
+    res.send(data)
+})
+
 app.get("/", (req,res) => {
     res.send("<h1>Hello Node App</h1>");
 })
-
 
 app.listen(8989, () => {
     console.log(`Server is running on 8989 port`);
